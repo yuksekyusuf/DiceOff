c4a27e8f3ce314b23551fdbff19e132bbf4bb08a
yuksekyusuf <68251921+yuksekyusuf@users.noreply.github.com>
2021-12-26 15:11:37 -0500

    Initial Commit

---
+++ DiceOff/DiceOffApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceOffApp.swift
+//  DiceOff
+//
+//  Created by Ahmet Yusuf Yuksek on 12/26/21.
+//
+
+import SwiftUI
+
+@main
+struct DiceOffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
